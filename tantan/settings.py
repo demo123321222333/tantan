2c7cfce128559c3796b6353d4cb952186814daa2
liye <1228906024@qq.com>
2019-10-27 18:36:28 +0800

    更改settings.py文件错误信息

--- tantan/settings.py
+++ tantan/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': tantan,
+        'NAME': 'tantan',
         'PORT':3306,
         'USER':'root',
         'PASSWORD':'123456',
